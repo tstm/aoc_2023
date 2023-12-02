26c3e887127746c8e1ced04d35e943a25d0919af
Martti Leppänen <tstm@softcon.fi>
2023-12-02 22:02:02 +0200

    Also add the criterion file

---
+++ day2/benches/benchmarks-criterion.rs
@@ -0,0 +1,27 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use day2::*;
+
+fn criterion_benchmark_part1(c: &mut Criterion) {
+    let input = include_str!("../input.txt");
+
+    let mut group = c.benchmark_group("day-02::part1");
+    group.bench_with_input("part1", input, |b, input| b.iter(|| part1::part1(input)));
+
+    group.finish();
+}
+
+fn criterion_benchmark_part2(c: &mut Criterion) {
+    let input = include_str!("../input.txt");
+
+    let mut group = c.benchmark_group("day-02::part2");
+    group.bench_with_input("part2", input, |b, input| b.iter(|| part2::part2(input)));
+
+    group.finish();
+}
+
+criterion_group!(
+    benches,
+    criterion_benchmark_part1,
+    criterion_benchmark_part2
+);
+criterion_main!(benches);
