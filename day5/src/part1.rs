15b43a5cd8847afed5393675fe1acd0bc00935ac
Martti Leppänen <martti.leppanen@supermetrics.com>
2023-12-06 02:15:45 +0200

    Initial version that gives the right answer

---
+++ day5/src/part1.rs
@@ -0,0 +1,117 @@
+use core::ops::Range;
+
+struct Seed(usize);
+
+#[derive(Debug)]
+struct ConversionMap {
+    from_range: Range<usize>,
+    to_range: usize,
+}
+
+impl ConversionMap {
+    fn convert(&self, id: usize) -> Option<usize> {
+        // if let Some(pos) = self.from_range.rposition(|s| s == id) {
+        //     eprintln!(
+        //         "Position {}, id {}, range {:?}",
+        //         &pos, &id, &self.from_range
+        //     );
+        //     Some(self.to_range.start + pos)
+        // } else {
+        //     None
+        // }
+
+        self.from_range
+            .clone()
+            .enumerate()
+            .find_map(|(n, from_id)| {
+                if from_id == id {
+                    Some(self.to_range + n)
+                } else {
+                    None
+                }
+            })
+    }
+}
+
+// struct SoilToFertilizer {
+//     seed_range: Range<usize>,
+//     fertilizer_range: Range<usize>,
+// }
+//
+// struct FertilizerToWater {
+//     seed_range: Range<usize>,
+//     fertilizer_range: Range<usize>,
+// }
+//
+// struct WaterToLight {
+//     seed_range: Range<usize>,
+//     fertilizer_range: Range<usize>,
+// }
+//
+// struct WaterToLight {
+//     seed_range: Range<usize>,
+//     fertilizer_range: Range<usize>,
+// }
+//
+
+fn parse_seeds(input: &str) -> Vec<Seed> {
+    input
+        .split_once(": ")
+        .expect("seeds: should be included in the string")
+        .1
+        .split_whitespace()
+        .map(|n| Seed(n.parse::<usize>().expect("Parse number failed")))
+        .collect::<Vec<_>>()
+}
+
+// Takes in just the numbers
+// fn parse_conversion_map(input: &str) -> Vec<ConversionMap> {
+//     input
+//         .lines()
+//         .map(|s| {
+//             let params: Vec<usize> = s
+//                 .split_whitespace()
+//                 .map(|n| n.parse::<usize>().expect("Parse conversion number failed"))
+//                 .collect();
+//             ConversionMap {
+//                 from_range: params[0]..(params[0] + params[2]),
+//                 to_range: params[1]..(params[1] + params[2]),
+//             }
+//         })
+//         .collect()
+// }
+
+fn parse_conversion_map(input: &str) -> ConversionMap {
+    let params: Vec<usize> = input
+        .split_whitespace()
+        .map(|n| n.parse::<usize>().expect("Parse conversion number failed"))
+        .collect();
+    ConversionMap {
+        to_range: params[0],
+        from_range: params[1]..(params[1] + params[2]),
+    }
+}
+
+pub fn part1(input: &str) -> Result<usize, String> {
+    // let seeds = lineiter.next().unwrap();
+    let chunks: Vec<_> = input.split_terminator("\n\n").collect();
+    let seeds = parse_seeds(chunks[0]);
+
+    let mut maps: Vec<Vec<ConversionMap>> = Vec::new();
+    for chunk in chunks[1..8].into_iter() {
+        let mut iter = chunk.lines().into_iter();
+        let _header = iter.next();
+        maps.push(iter.map(|c| parse_conversion_map(c)).collect());
+    }
+
+    Ok(seeds
+        .iter()
+        .map(|seed| {
+            maps.iter().fold(seed.0, |s, m| {
+                m.iter().find_map(|x| x.convert(s)).unwrap_or(s)
+            })
+        })
+        .inspect(|f| println!("Result: {}", f))
+        .min()
+        .expect("No value found?"))
+}
---
+++ day5/src/part2.rs
@@ -0,0 +1,108 @@
+// use core::ops::Range;
+use itertools::Itertools;
+use range_set_blaze::prelude::*;
+
+// struct Seed(Range<usize>);
+
+#[derive(Debug)]
+struct ConversionMap {
+    start: isize,
+    end: isize,
+    diff: isize,
+}
+
+impl ConversionMap {
+    pub fn parse(input: &str) -> ConversionMap {
+        let params: Vec<isize> = input
+            .split_whitespace()
+            .map(|n| n.parse::<isize>().expect("Parse conversion number failed"))
+            .collect();
+        ConversionMap {
+            start: params[1],
+            end: params[1] + params[2],
+            diff: params[0] - params[1],
+        }
+    }
+
+    fn convert(&self, range: RangeSetBlaze<isize>) -> (RangeSetBlaze<isize>, RangeSetBlaze<isize>) {
+        let mut bottom = range;
+        let mut mid = bottom.split_off(self.start);
+        let top = mid.split_off(self.end);
+        let mapped = mid
+            .ranges()
+            .map(|r| ((r.start() + self.diff)..=(r.end() + self.diff)))
+            .collect();
+        let remains = top | bottom;
+        (remains, mapped)
+    }
+}
+
+struct ConversionLayer {
+    maps: Vec<ConversionMap>,
+    name: String,
+}
+
+impl ConversionLayer {
+    fn new(input: &str) -> ConversionLayer {
+        let mut iter = input.lines().into_iter();
+        let name = iter.next().expect("Should be topic string").to_string();
+        let maps = iter.map(|c| ConversionMap::parse(c)).collect();
+
+        ConversionLayer { name, maps }
+    }
+
+    fn convert(&self, input: RangeSetBlaze<isize>) -> RangeSetBlaze<isize> {
+        let mut current = input.clone();
+        let mut output = vec![];
+        for map in self.maps.iter() {
+            let (remains, mapped) = map.convert(current);
+            output.push(mapped);
+            current = remains;
+        }
+        output.push(current);
+        output.union()
+    }
+}
+
+fn parse_seeds(input: &str) -> RangeSetBlaze<isize> {
+    let mut seeds: Vec<RangeSetBlaze<isize>> = vec![];
+    let mut values = input
+        .split_once(": ")
+        .expect("seeds: should be included in the string")
+        .1
+        .split_whitespace()
+        .map(|n| n.parse::<isize>().expect("Parse number failed"))
+        // .collect::<Vec<_>>()
+        .tuples();
+
+    while let Some((start, length)) = values.next() {
+        seeds.push(RangeSetBlaze::from_iter(start..(start + length)))
+    }
+    seeds.union()
+}
+
+pub fn part2(input: &str) -> Result<isize, String> {
+    // let seeds = lineiter.next().unwrap();
+    let chunks: Vec<_> = input.split_terminator("\n\n").collect();
+    let seeds = parse_seeds(chunks[0]);
+    let mut layers: Vec<ConversionLayer> = vec![];
+
+    for chunk in chunks[1..8].into_iter() {
+        let l = ConversionLayer::new(chunk);
+        layers.push(l);
+    }
+
+    assert_eq!(layers.len(), 7);
+
+    let soil = layers[0].convert(seeds.clone());
+    let fertilizer = layers[1].convert(soil);
+    let water = layers[2].convert(fertilizer);
+    let light = layers[3].convert(water);
+    let temperature = layers[4].convert(light);
+    let humidity = layers[5].convert(temperature);
+    let location = layers[6].convert(humidity);
+
+    let minimum = location.ranges().map(|range| *range.start()).min().unwrap();
+
+    Ok(minimum)
+}
