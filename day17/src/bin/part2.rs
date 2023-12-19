39dd1616859689d32c24392d77575814b20d8b3b
Martti Leppänen <tstm@softcon.fi>
2023-12-20 00:02:09 +0200

    Added some dhat testing on day17

--- day17/src/bin/part2.rs
+++ day17/src/bin/part2.rs
@@ -1,6 +1,13 @@
 use day17::part2::run;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 pub fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     let input = include_str!("../../input.txt");
     let result = run(input).unwrap();
     println!("Output: {}", result);
